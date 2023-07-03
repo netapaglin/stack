932449d87c68be0b49f607cec4893141fdfde49c
Neta Paglin <netapaglin@gmail.com>
2023-07-03 10:49:10 +0300

    first commit

---
+++ stack.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "ADTDefs.h"
+#include "stack.h"
+
+struct Stack
+{
+	Vector *m_vector;
+	int m_magicNum;
+};
---
+++ stackTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "ADTDefs.h"
+#include "stack.h"
+
+
+int main (void)
+{
+
+ putchar("v");
+ 
+	return ERR_OK;
+}
